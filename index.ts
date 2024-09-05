ec077e5966661b474c6d9a67fac5cb0baaee4893
Györfi Marcell <44555742+gyorfimarcell@users.noreply.github.com>
2024-09-05 10:46:15 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+const daySelectorEl = document.getElementById("daySelector") as HTMLSelectElement;
+const daysEl = document.getElementById("days") as HTMLOListElement;
+
+const monthNames = ["Január",
+    "Február",
+    "Március",
+    "Április",
+    "Május",
+    "Június",
+    "Július",
+    "Augusztus",
+    "Szeptember",
+    "Október",
+    "November",
+    "December"];
+
+function AddSelectOptions() {
+    for (let index = 0; index < 12; index++) {
+
+
+    }
+}
